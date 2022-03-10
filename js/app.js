197896a06e262d8cbeeeb0e5ef17fc6222fb16e3
shagors <shajjadshagor@gmail.com>
2022-03-11 00:21:44 +0600

    love button duplicatig fixed

--- js/app.js
+++ js/app.js
@@ -36,12 +36,14 @@ const switchTab = (id) => {
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
   } else if (id === "liked") {
+    document.getElementById("liked").innerHTML = "";
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
 
     displayLikedPosts();
   } else {
+    document.getElementById("reported").innerHTML = "";
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
