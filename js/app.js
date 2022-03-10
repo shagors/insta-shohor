b539c21bb24666e8c74297b00107f79c18cb6869
shagors <shajjadshagor@gmail.com>
2022-03-10 23:12:22 +0600

    user Image solved

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">${post.comments[0].user}</a>
                 </div>
